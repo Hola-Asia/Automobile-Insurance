df914a8d0c00803a6298bf22e84e9bfe88b1ace6
Hola <1264590756@qq.com>
2020-12-25 10:28:55 +0800

    提交封装axios

---
+++ src/units/http.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+const http=axios.create({
+    baseURL:'',//如果你配置了，那么所有的url前面都会拼接一个    baseURL
+    timeout:10000, //过期时间  ajax请求超多这个时间自动中断
+    headers:{
+        "Content-Type":"application/json"
+    }
+});
+export default http;
