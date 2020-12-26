ee528e2aa76ae96752b69f0943b2716d47a998ae
Liu <844260278@qq.com>
2020-12-26 16:06:26 +0800

    添加登陆页

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,43 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Navigation from '../components/navigation.vue'
-import Information from '../views/Information.vue'
-import Order from '../views/Order.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Navigation from '../components/navigation.vue';
+import Information from '../views/Information.vue';
+import Order from '../views/Order.vue';
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    redirect: {name: 'Navigation'},
-  },
-  {
-    path: '/navigation',
-    name: 'Navigation',
-    component: Navigation,
-    children: [
-      {
-        // 资讯页接口
-        path: '/information',
-        name: 'Information',
-        component: Information
-      },
-      {
-        // 订单管理页接口
-        path: '/order',
-        name: 'Order',
-        component: Order
-      }
-    ]
-  },
+const routes = [{
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/',
+        redirect: { name: 'Login' },
+    },
+    {
+        path: '/navigation',
+        name: 'Navigation',
+        component: Navigation,
+        children: [{
+                // 资讯页接口
+                path: '/information',
+                name: 'Information',
+                component: Information
+            },
+            {
+                // 订单管理页接口
+                path: '/order',
+                name: 'Order',
+                component: Order
+            }
+        ]
+    },
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div>
+            <h1>汽 车 保 险 后 台 管 理 系 统</h1>
+            <el-input placeholder="请输入用户名" v-model="user" clearable></el-input>
+            <el-input type="password" placeholder="请输入密码" v-model="password" clearable></el-input>
+            <el-row>
+            <el-button>登陆</el-button>
+            </el-row>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        user: '',
+        password:''
+      }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
