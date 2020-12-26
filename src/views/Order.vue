9b6d05480b11316c2bc79c14cfa654ff8b5eb2da
Hola <1264590756@qq.com>
2020-12-26 16:50:07 +0800

    no message

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -19,16 +19,43 @@
         </el-row>
       </el-col>
     </el-row>
+    <el-row>
+      <el-dropdown trigger="click" @command="changeSel">
+        <span class="el-dropdown-link">{{sel}}<i class="el-icon-arrow-down el-icon--right"></i></span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item v-for="(v,i) in selArr" :command = i>{{v}}</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+      <el-input v-model="selName" class="inputName" :placeholder = placeStr></el-input>
+      <el-input v-model="selName" class="inputPrice" placeholder = '保费金额-最小'>￥</el-input> -
+      <el-input v-model="selName" class="inputPrice" placeholder = '保费金额-最大'></el-input>
+    </el-row>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Order"
+  name: "Order",
+  data:function(){
+    return {
+      //下拉框数据
+      placeStr:'请选择筛选方式',
+      sel:'筛选方式',
+      selArr:['订单编号','车主名称','手机号码','车架号','车牌号'],
+      selName:''
+    }
+  },
+  methods:{
+    //下拉框选择
+    changeSel(i){
+      this.sel = this.selArr[i];
+      this.placeStr = `请输入${this.selArr[i]}`;
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
   .letter{
     float: left;
   }
@@ -36,5 +63,28 @@ export default {
     content: '';
     display: block;
     clear: both;
+  }
+  .el-row{
+    text-align: left;
+    line-height: 50px;
+  }
+  .el-dropdown{
+    >span{
+      cursor: pointer;
+    }
+  }
+  .el-dropdown-link {
+    cursor: pointer;
+    color: #409EFF;
+  }
+  .el-icon-arrow-down {
+    font-size: 12px;
+  }
+  .inputName{
+    width: 200px;
+    margin: 0 15px;
+  }
+  .inputPrice{
+    width: 150px;
   }
 </style>
