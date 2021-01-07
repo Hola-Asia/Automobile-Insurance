5bfd73dc7d211b06f9bb1612bcb6ab542966fdd3
Liu <844260278@qq.com>
2021-01-07 20:02:41 +0800

    登陆成功跳转订单管理模块

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -106,7 +106,7 @@ export default {
           }else{
             sessionStorage.token=res.data.data.token;
             sessionStorage.jurisdiction=JSON.stringify(res.data.data.menus);
-            this.$router.push('/guaranteeSlipList');
+            this.$router.push('/order');
             this.open2('登录成功');
           }
         }).catch((err)=>{
