dc7ff4e5e88d03309cd86fe08ed7d565d6095a74
Liu <844260278@qq.com>
2021-01-07 18:49:31 +0800

    配置公共请求头token值

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import axios from 'axios'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import axios from 'axios';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import http from './units/http';
